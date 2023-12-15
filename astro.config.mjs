32d52792118c18b0f9cf2d5fb05878e181f19ed6
Flowers <floresfernandezalexis05@gmail.com>
2023-12-15 15:56:25 -0600

    Add Preact integration and Greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexisflo.netlify.app/"
+  site: "https://alexisflo.netlify.app/",
+  integrations: [preact()]
 });
